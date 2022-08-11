e89090aa6d547866d20186ebf8fe5a3bbeae6be5
latonv <1619661+latonv@users.noreply.github.com>
2022-08-11 12:45:08 -0700

    Update demo to show input events and styling

--- demo/app-root.ts
+++ demo/app-root.ts
@@ -1,11 +1,18 @@
 import { html, css, LitElement } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import { customElement, query, state } from 'lit/decorators.js';
 import '../src/clearable-text-input';
+import type { ClearableTextInput } from '../src/clearable-text-input';
 
 @customElement('app-root')
 export class AppRoot extends LitElement {
   @state()
+  private lastInputResult = '';
+
+  @state()
   private lastClearResult = '';
+
+  @query('clearable-text-input')
+  private widget!: ClearableTextInput;
 
   render() {
     const params = new URLSearchParams(window.location.search);
@@ -16,12 +23,26 @@ export class AppRoot extends LitElement {
         .placeholder=${'Search...'}
         .screenReaderLabel=${'Search'}
         @clear=${this.onClear}
+        @input=${this.onInput}
       >
       </clearable-text-input>
-      <p ?hidden=${!this.lastClearResult}>
-        Value prior to clearing was:
-        <span id="clear-result">${this.lastClearResult}</span>
-      </p>
+      <div id="controls">
+        <label>
+          <input
+            type="checkbox"
+            @change=${this.toggleDarkMode}
+          >
+          &nbsp;Simulate dark mode
+        </label>
+        <p>
+          Current component value (updated upon each input event):
+          <span id="input-result">${this.lastInputResult}</span>
+        </p>
+        <p ?hidden=${!this.lastClearResult}>
+          Component value prior to the last clear event was:
+          <span id="clear-result">${this.lastClearResult}</span>
+        </p>
+      </div>
     `;
   }
 
@@ -31,10 +52,28 @@ export class AppRoot extends LitElement {
     this.lastClearResult = preClearValue;
   }
 
+  private onInput() {
+    this.lastInputResult = this.widget.value;
+  }
+
+  private toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    this.widget.classList.toggle('dark-mode');
+  }
+
   static styles = css`
     :host {
       display: block;
       font-size: 1.6rem;
+    }
+
+    #controls {
+      margin-top: 0.8rem;
+    }
+
+    clearable-text-input.dark-mode {
+      --input-background-color: #2c2c2c;
+      --input-color: #fff;
     }
   `;
 }
